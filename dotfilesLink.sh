21e27d4d48d5056170758861f1cb63d20364288d
kazuki matsumoto <kazuki.matsumoto@vasily.jp>
2015-10-21 09:32:07 +0900

    dotfilesnにパスを書き換え

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ln -sf ~/my_dotfiles/vimrc ~/.vimrc
-ln -sf ~/my_dotfiles/vimrc.bundles ~/.vimrc.bundles
-ln -sf ~/my_dotfiles/zshrc ~/.zshrc
-ln -sf ~/my_dotfiles/zshenv ~/.zshenv
-ln -sf ~/my_dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vimrc.bundles ~/.vimrc.bundles
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshenv ~/.zshenv
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
