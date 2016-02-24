5fc3ffd89705ac9cfee90059a8236302700a24b1
kazuki matsumoto <kazk1210@gmail.com>
2016-02-24 19:11:23 +0200

    apply tmux navigator

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/vimrc.bundles ~/.vimrc.bundles
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zshenv ~/.zshenv
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
